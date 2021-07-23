218072a8b1b5bb38dae52841f831e97382f0aa74
Hidetaka Okamoto <info@wp-kyoto.net>
2021-07-24 00:36:45 +0900

    feat: force fetch button

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
-import { useEffect, useState } from 'react';
-import { Container, Navbar } from 'react-bootstrap';
-import {StripeTOPCustomer} from './interfaces';
+import { useCallback, useEffect, useState } from 'react';
+import { Container, Navbar, Form, Button } from 'react-bootstrap';
+import { StripeTOPCustomer } from './interfaces';
 import { CustomerTable } from './Table';
 
 
@@ -9,12 +9,14 @@ import { CustomerTable } from './Table';
  * @param path 
  * @returns 
  */
-const getAPIURL = (path: string = '' ) => {
+const getAPIURL = (path: string = '', queryString: string = '') => {
   const paths = [ 
     process.env.REACT_APP_API_URL || 'http://localhost:3100',
     path.replace(/^\//, '')
   ]
-  return paths.filter(Boolean).join('/')
+  const api = paths.filter(Boolean).join('/')
+  if (!queryString) return api
+  return `${api}?${queryString}`
 }
 
 
@@ -22,12 +24,10 @@ function App() {
   const [fetchStatus, setFetchStatus] = useState<'' | 'loading' | 'success' | 'failed'>('')
   const [customers, setCustomers] = useState<StripeTOPCustomer[]>([]) 
 
-  /**
-   * Fetch API to load customers
-   */
-  useEffect(() => {
+  const fetchAPI = useCallback(async (avoidCache = false) => {
+    const api = getAPIURL('top_customers', avoidCache ? `avoid-cache-key=${Math.random() * 100}`: '')
     setFetchStatus('loading')
-    fetch(getAPIURL('top_customers'))
+    fetch(api)
       .then(function(response) {
         return response.json()
       }).then(function(json) {
@@ -37,12 +37,25 @@ function App() {
         console.log('parsing failed', ex)
         setFetchStatus('failed')
       })
-  }, [setFetchStatus])
+  }, [setCustomers, setFetchStatus])
+  /**
+   * Fetch API to load customers
+   */
+  useEffect(() => {
+    fetchAPI()
+  }, [fetchAPI])
 
   return (
     <div className="App">
       <Navbar expand="lg">
         <Navbar.Brand>Stripe Customer insights</Navbar.Brand>
+        <div className="mr-auto"/>
+          <Form inline onSubmit={(e) => {
+            e.preventDefault()
+            fetchAPI(true)
+          }}>
+            <Button variant="outline-primary" type="submit">Re fetch</Button>
+          </Form>
       </Navbar>
       <Container>
         {fetchStatus === 'loading' ? 'Loading...' : (
